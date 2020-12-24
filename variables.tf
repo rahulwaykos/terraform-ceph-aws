1bfca3da26e271a59d1044818556cdbf1c194df0
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-12-25 01:21:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,16 @@ variable "key_name" {
 variable "private_key_path" {
   default  = "~/terraform/storage-gateway.pem"
 }
+
+variable "cluster_member_count" {
+  description = "Number of members in the cluster"
+  default = "3"
+}
+variable "cluster_member_name_prefix" {
+  description = "Prefix to use when naming cluster members"
+  default = "cluster-node-"
+}
+variable "aws_keypair_privatekey_filepath" {
+  description = "Path to SSH private key to SSH-connect to instances"
+  default = "./secrets/aws.key"
+}
